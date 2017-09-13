3fc02df60055be9c1fd68d3ca24ee313c505fe73
qinting513 <513924632@qq.com>
2017-09-13 21:36:34 +0800

    new create project

---
+++ PandaTV/PandaTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PandaTV
+//
+//  Created by QinTing on 2017/9/13.
+//  Copyright © 2017年 QinTing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
