245366b54a397c2f904e227e0058be6e71b19a1c
qinting513 <513924632@qq.com>
2017-09-14 20:29:34 +0800

    搭建项目架构

---
+++ PandaTV/PandaTV/Classes/Base/ViewController/BaseVC.swift
@@ -0,0 +1,35 @@
+//
+//  BaseVC.swift
+//  PandaTV
+//
+//  Created by QinTing on 2017/9/14.
+//  Copyright © 2017年 QinTing. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
